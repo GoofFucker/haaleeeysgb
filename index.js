e40297f51736eb03a798ba1ad25b98fc5067e302
GoofFucker <47990107+GoofFucker@users.noreply.github.com>
2019-03-01 23:10:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ client.on('guildMemberRemove', member => {
     var ayy2 = client.emojis.find(emoji => emoji.name === "x");
     var us2 = client.emojis.find(emoji => emoji.name === "flag_us");
     var fr2 = client.emojis.find(emoji => emoji.name === "flag_fr");
-    member.guild.channels.get('549914111492882442').send(us2 + " Goodbye " + member + " Thanks for coming ! " + ayy2);
-    member.guild.channels.get('549914111492882442').send(fr2 + " Au-revoir " + member + " Merci d'être passé ! " + ayy2);
+    member.guild.channels.get('551164240786882601').send(us2 + " Goodbye " + member + " Thanks for coming ! " + ayy2);
+    member.guild.channels.get('551164240786882601').send(fr2 + " Au-revoir " + member + " Merci d'être passé ! " + ayy2);
     member.send('Hey, si tu souhaites revenir nous voir ^^\n https://discord.gg/Qwkcg8b');
     console.log("SendLeave"); 
 });
@@ -27,8 +27,8 @@ client.on('guildMemberAdd', member => {
     var ayy = client.emojis.find(emoji => emoji.name === "tada");
     var us = client.emojis.find(emoji => emoji.name === "flag_us");
     var fr = client.emojis.find(emoji => emoji.name === "flag_fr");
-    member.guild.channels.get('549914111492882442').send(us2 + " Hi " + member + " Welcome to **NegativeNT** ! " + ayy);
-    member.guild.channels.get('549914111492882442').send(fr2 + " Salut " + member + " Bienvenue sur **NegativeNT** ! " + ayy);
+    member.guild.channels.get('551164240786882601').send(us2 + " Hi " + member + " Welcome to **NegativeNT** ! " + ayy);
+    member.guild.channels.get('551164240786882601').send(fr2 + " Salut " + member + " Bienvenue sur **NegativeNT** ! " + ayy);
     member.send('Hey, ce message n\'est pas terminé');
     console.log("SendJoin"); 
 });
